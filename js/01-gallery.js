a9d0185a15e22cdc0f415933df7eaefb862bad1d
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2023-03-23 22:06:06 +0200

    ready

---
+++ js/01-gallery.js
@@ -0,0 +1,33 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector(".gallery");
+const galleryItemsImages = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`).join("");
+
+gallery.insertAdjacentHTML('beforeend', galleryItemsImages);
+
+gallery.addEventListener('click', openImgBigFoto);
+
+function openImgBigFoto(e) {
+    e.preventDefault();
+
+    const img = e.target.dataset.source;
+
+    const instance = basicLightbox.create(`<img src="${img}" width="800" height="600" />`);
+    instance.show()
+
+    window.addEventListener('keydown', e => {
+        if (e.code === "Escape") {
+            instance.close();
+        }
+    })
+};
+
+
